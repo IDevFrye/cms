d7fdd730bc493cf04ec22a351dd759b83cb863dd
Andrey Kostin <andrey.kostin.111@mail.ru>
2025-01-09 00:31:09 +0300

    [M2][TASK-5] Init wrapper

--- script/cms.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const modalTitle = document.querySelector('.modal__title');
-const modalForm = document.querySelector('.modal__form');
-const modalDiscountCheckbox = document.querySelector('.modal__checkbox');
-const modalDiscountInput = document.querySelector('.modal__input_discount');
