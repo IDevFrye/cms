864c33f0f6e5edaf4d5f91d9dbd768e7161b63f7
Andrey Kostin <andrey.kostin.111@mail.ru>
2025-01-09 03:06:59 +0300

    [M2][TASK-6] Fix close modal + add delete goods handler

--- script/cms-script.js
+++ script/cms-script.js
@@ -3,7 +3,7 @@
 {
   const jsonData = `[
     {
-      "id": 3,
+      "id": 234324234324545,
       "title": "Смартфон Xiaomi 11T 8/128GB",
       "price": 27000,
       "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
@@ -17,7 +17,7 @@
       }
     },
     {
-      "id": 4,
+      "id": 234324234324546,
       "title": "Радиоуправляемый автомобиль Cheetan",
       "price": 4000,
       "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
@@ -31,7 +31,7 @@
       }
     },
     {
-      "id": 5,
+      "id": 234324234324547,
       "title": "ТВ приставка MECOOL KI",
       "price": 12400,
       "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
@@ -45,7 +45,7 @@
       }
     },
     {
-      "id": 6,
+      "id": 234324234324548,
       "title": "Витая пара PROConnect 01-0043-3-25",
       "price": 22,
       "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
@@ -60,12 +60,12 @@
     }
   ]`;
 
-  const createRow = (item) => {
+  const createRow = (item, index) => {
     const tr = document.createElement('tr');
     tr.insertAdjacentHTML('afterbegin', `
-      <td class="table__cell">${item.id}</td>
-      <td class="table__cell table__cell_left table__cell_name" data-id="${item.dataId}">
-        <span class="table__cell-id">id: ${item.dataId}</span>${item.title}</td>
+      <td class="table__cell">${index + 1}</td>
+      <td class="table__cell table__cell_left table__cell_name" data-id="${item.id}">
+        <span class="table__cell-id">id: ${item.id}</span>${item.title}</td>
       <td class="table__cell table__cell_left">${item.category}</td>
       <td class="table__cell">${item.units}</td>
       <td class="table__cell">${item.count}</td>
@@ -81,8 +81,9 @@
 
   const renderGoods = (objArr) => {
     const tbody = document.querySelector('.table__body');
-    objArr.forEach((item) => {
-      tbody.append(createRow(item));
+    tbody.innerHTML = '';
+    objArr.forEach((item, index) => {
+      tbody.append(createRow(item, index));
     });
   };
 
@@ -93,8 +94,8 @@
     const modalDiscountInput = document.querySelector('.modal__input_discount');
     const overlay = document.querySelector('.overlay');
     const modalWindow = document.querySelector('.modal');
-    const modalClose = document.querySelector('.modal__close');
     const btnAdd = document.querySelector('.panel__add-goods');
+    const tableBody = document.querySelector('.table__body');
 
     const toggleModal = () => {
       overlay.classList.toggle('active');
@@ -106,10 +107,27 @@
     renderGoods(goods);
 
     btnAdd.addEventListener('click', toggleModal);
-    overlay.addEventListener('click', toggleModal);
-    modalClose.addEventListener('click', toggleModal);
-    modalWindow.addEventListener('click', (event) => {
-      event.stopPropagation();
+    overlay.addEventListener('click', e => {
+      const target = e.target;
+      if (target === overlay || target.closest('.modal__close')) {
+        toggleModal();
+      };
+    });
+    tableBody.addEventListener('click', e => {
+      const target = e.target;
+      if (target.classList.contains('table__btn_del')) {
+        const row = target.closest('tr');
+        const rowId = parseInt(row.children[1].dataset.id);
+        row.remove();
+
+        const indexToRemove = goods.findIndex(item => item.id === rowId);
+        if (indexToRemove !== -1) {
+          goods.splice(indexToRemove, 1);
+        };
+        renderGoods(goods);
+
+        console.log('Обновлённые данные:', goods);
+      };
     });
   };
 
